9a8b76159e0308499c80095dd2b58517eac2ec81
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-20 18:46:24 +0500

    feat: Add File entity class

---
+++ Drive.Entities/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Entities
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int ParenFolderId { get; set; }
+        public String FileExt { get; set; }
+        public int FileSizeInKB { get; set; }
+        public DateTime UploadedOn { get; set; }
+        public Boolean IsActive { get; set; }
+    }
+}
