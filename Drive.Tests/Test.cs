037efff009bf2f4a10fd24b0c9b4620a69400b09
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-20 15:23:50 +0500

    Initial Commit

---
+++ Drive.Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Drive.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void AlwaysFalse()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
