b7d73d81a673ef1adeff0732a5c9b31da3d5acdc
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-20 18:43:35 +0500

    feat: Add Folder entity class

---
+++ Drive.Entities/Folder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Entities
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int ParentFolderId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Boolean IsActive { get; set; }
+    }
+}
