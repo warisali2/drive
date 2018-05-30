414228964f2c8973031d4d8296c1c732bf2f033f
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-30 17:24:20 +0500

    feat: Add IntegrationTestsBase class

---
+++ Drive.DAL.Tests/IntegrationTests/IntegrationTestsBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL.Tests.IntegrationTests
+{
+    public class IntegrationTestsBase
+    {
+        String _conStringName = "SQL_SERVER_TEST_DB_CON_STRING";
+        DbContext _context = null;
+
+        public IntegrationTestsBase()
+        {
+            _context = CreateDbContext();
+        }
+
+        public IntegrationTestsBase(String key)
+        {
+            _conStringName = key;
+            _context = CreateDbContext();
+        }
+
+        private DbContext CreateDbContext()
+        {
+            return new DbContext(_conStringName);
+        }
+
+    }
+}
