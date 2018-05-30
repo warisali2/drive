06eb54592c4a6c96e4bc64a512644d5b1f29a13e
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-30 09:56:46 +0500

    feat: Add IDAO interface

---
+++ Drive.DAL/IDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL
+{
+    public interface IDAO<T>
+    {
+        int Insert(T obj);
+        int Update(T obj);
+        T GetById(int id);
+        List<T> GetAll();
+        int Delete(int id);
+        T Map(SqlDataReader reader);
+
+    }
+}
