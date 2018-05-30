95224c49229145c398067a3bbc1de618f476d87d
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-30 10:32:06 +0500

    feat: Add AddParameter Extension method to IDbCommand

---
+++ Drive.DAL/IDbCommandExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL
+{
+    public static class IDbCommandExtension
+    {
+        public static void AddParameter(this IDbCommand command, string name, object value)
+        {
+            if (command == null) throw new ArgumentNullException("command");
+            if (name == null) throw new ArgumentNullException("name");
+
+            var p = command.CreateParameter();
+
+            p.ParameterName = name;
+            p.Value = value ?? DBNull.Value;
+
+            command.Parameters.Add(p);
+        }
+    }
+}
