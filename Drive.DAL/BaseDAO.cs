79400ca6f9534432f9a4c47c87322ac0508de971
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-30 17:18:25 +0500

    feat: Add BaseDAO class to expose DbContext

---
+++ Drive.DAL/BaseDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL
+{
+    public class BaseDAO
+    {
+        protected DbContext _context;
+
+        public BaseDAO()
+        {
+            _context = new DbContext();
+        }
+
+        public BaseDAO(String key)
+        {
+            _context = new DbContext(key);
+        }
+
+    }
+}
