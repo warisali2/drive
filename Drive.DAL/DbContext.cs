43927ddbd93c7c2e98187003cddd16adc9503209
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-30 09:40:17 +0500

    feat: Add DbContext class in Drive.DAL

---
+++ Drive.DAL/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL
+{
+    internal class DbContext : IDisposable
+    {
+        String _conString = ConfigurationManager.ConnectionStrings["SQL_SERVER_CON_STRING"].ConnectionString;
+        SqlConnection _connection = null;
+
+        public DbContext()
+        {
+            _connection = new SqlConnection(_conString);
+            _connection.Open();
+        }
+        public void Dispose()
+        {
+            if (_connection != null && _connection.State == System.Data.ConnectionState.Open)
+                _connection.Close();
+        }
+    }
+}
