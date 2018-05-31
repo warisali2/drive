156d4d4ad2c9e7aefb5ddc814730aba4a52e6709
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-31 11:47:45 +0500

    feat: Add tests for GetAll

--- Drive.DAL.Tests/IntegrationTests/FileDAOIntegrationTests.cs
+++ Drive.DAL.Tests/IntegrationTests/FileDAOIntegrationTests.cs
@@ -22,5 +22,36 @@ namespace Drive.DAL.Tests.IntegrationTests
         {
             dao = new FileDAO(key);
         }
+
+
+        [Test]
+        public void GetAll__WithNoRows__ReturnsListOfZero()
+        {
+            var list = dao.GetAll();
+
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [Test]
+        public void GetAll__InsertOneRow__ReturnsListOfOne()
+        {
+            File file = new File();
+            file.Name = "Test File";
+            file.ParenFolderId = -1;
+            file.IsActive = true;
+            file.FileExt = ".txt";
+            file.FileSizeInKB = 100;
+            file.CreatedBy = 1;
+            file.UploadedOn = DateTime.Now;
+
+            file.Id = dao.Insert(file);
+
+            var list = dao.GetAll();
+
+            Assert.AreEqual(1, list.Count);
+        }
+
+        
+
     }
 }
