59f76c1166adade890679bdfe55fe16fe8cc68e5
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-05-31 10:10:58 +0500

    feat: Add GetAll__WithNoRows__ReturnsListOfZero() test

---
+++ Drive.DAL.Tests/IntegrationTests/FileDAOIntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class FileDAOIntegrationTests : IntegrationTestsBase
+    {
+        [Test]
+        public void GetAll__WithNoRows__ReturnsListOfZero()
+        {
+            FileDAO dao = new FileDAO();
+
+            var list = dao.GetAll();
+
+            Assert.AreEqual(list.Count, 0);
+        }
+    }
+}
