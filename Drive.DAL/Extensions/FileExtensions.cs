2647625886ae6737bf00c9f07cf2482b5eac57d7
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-03 10:58:17 +0500

    feat: Add Delete, Save, and Update extension methods to File entity class

---
+++ Drive.DAL/Extensions/FileExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Drive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL.Extensions
+{
+    public static class FileExtensions
+    {
+        public static int Delete(this File file)
+        {
+            FileDAO dao = new FileDAO();
+            return dao.Delete(file.Id);
+        }
+
+        public static int Save(this File file)
+        {
+            FileDAO dao = new FileDAO();
+            return dao.Insert(file);
+        }
+
+        public static int Update(this File file)
+        {
+            FileDAO dao = new FileDAO();
+            return dao.Update(file);
+        }
+    }
+}
