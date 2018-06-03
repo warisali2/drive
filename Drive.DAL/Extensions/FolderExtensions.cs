80c16b9367cf4ca84ef2820b89fe3b275564395d
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-03 10:59:16 +0500

    feat: Add Delete, Save, and Update extension methods to Folder entity class

---
+++ Drive.DAL/Extensions/FolderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Drive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL.Extensions
+{
+    public static class FolderExtensions
+    {
+        public static int Delete(this Folder Folder)
+        {
+            FolderDAO dao = new FolderDAO();
+            return dao.Delete(Folder.Id);
+        }
+
+        public static int Save(this Folder Folder)
+        {
+            FolderDAO dao = new FolderDAO();
+            return dao.Insert(Folder);
+        }
+
+        public static int Update(this Folder Folder)
+        {
+            FolderDAO dao = new FolderDAO();
+            return dao.Update(Folder);
+        }
+    }
+}
