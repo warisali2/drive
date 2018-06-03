bed49896e005f2b320ea67d7df163d3ca6eb769d
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-03 10:59:56 +0500

    feat: Add Delete, Save, and Update extension methods for User entity class

---
+++ Drive.DAL/Extensions/UserExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Drive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.DAL.Extensions
+{
+    public static class UserExtensions
+    {
+        public static int Delete(this User User)
+        {
+            UserDAO dao = new UserDAO();
+            return dao.Delete(User.Id);
+        }
+
+        public static int Save(this User User)
+        {
+            UserDAO dao = new UserDAO();
+            return dao.Insert(User);
+        }
+
+        public static int Update(this User User)
+        {
+            UserDAO dao = new UserDAO();
+            return dao.Update(User);
+        }
+    }
+}
