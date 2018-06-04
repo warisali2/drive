74fb3fbaedb125e652b8b0efd843ed8f31bafc9b
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 14:28:59 +0500

    feat: Add GetAllByUser in folders api

---
+++ Drive.API/Controllers/FoldersController.cs
@@ -0,0 +1,26 @@
+﻿using Drive.DAL;
+using Drive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Drive.API.Controllers
+{
+    public class FoldersController : ApiController
+    {
+        public List<Folder> GetAllOfUser(int id)
+        {
+            FolderDAO folderDAO = new FolderDAO();
+
+            var folders = folderDAO.GetAll();
+            var activeAndRootFolders = (from folder in folders
+                                        where folder.IsActive == true && folder.ParentFolderId == -1 && folder.CreatedBy == id
+                                        select folder) as List<Folder>;
+
+            return activeAndRootFolders;
+        }
+    }
+}
