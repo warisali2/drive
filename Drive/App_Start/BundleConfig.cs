5ad6e82d001fd0143b26e3b6a7c0758d10c6b068
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 11:21:34 +0500

    feat: Add uglipop js library

--- Drive/App_Start/BundleConfig.cs
+++ Drive/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Drive
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/uglipop").Include(
+                        "~/Scripts/uglipop.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
