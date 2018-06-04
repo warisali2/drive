ca637fcce8e13c44b253536e7733611e10ad9370
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 10:49:05 +0500

    feat: Show files and folders of specific user in home

--- Drive/Controllers/HomeController.cs
+++ Drive/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Drive.DAL;
+using Drive.Entities;
+using Drive.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,26 @@ namespace Drive.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        [AuthorizedOnly]
         public ActionResult Index()
         {
+            FolderDAO folderDAO = new FolderDAO();
+
+            var folders = folderDAO.GetAll();
+            var activeAndRootFolders = (from folder in folders
+                                        where folder.IsActive == true && folder.ParentFolderId == -1 && folder.CreatedBy == SessionManager.User.Id
+                                        select folder) as ICollection<Folder>;
+
+            FileDAO fileDAO = new FileDAO();
+
+            var files = fileDAO.GetAll();
+            var activeAndRootFiles = (from file in files
+                                      where file.IsActive == true && file.ParenFolderId == -1 && file.CreatedBy == SessionManager.User.Id
+                                      select file) as ICollection<File>;
+
+            ViewBag.files = activeAndRootFiles;
+            ViewBag.folder = activeAndRootFolders;
+
             return View();
         }
     }
