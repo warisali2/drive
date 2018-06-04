528214b2b324a5fbab872e51634db190bfb09405
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 09:20:28 +0500

    feat: Add SessionManager Class

---
+++ Drive/Security/SessionManager.cs
@@ -0,0 +1,48 @@
+﻿using Drive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drive.Security
+{
+    public static class SessionManager
+    {
+        public static User User
+        {
+            get
+            {
+                User dto = null;
+
+                if(HttpContext.Current.Session["User"] != null)
+                {
+                    dto = HttpContext.Current.Session["User"] as User;
+                }
+
+                return dto;
+            }
+
+            set
+            {
+                HttpContext.Current.Session["User"] = value;
+            }
+        }
+
+        public static Boolean IsValidUser
+        {
+            get
+            {
+                if (User != null)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        public static void ClearSession()
+        {
+            HttpContext.Current.Session.RemoveAll();
+            HttpContext.Current.Session.Abandon();
+        }
+    }
+}
