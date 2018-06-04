6fef33bf42d7e6630916141c2717d5837de75897
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 09:22:26 +0500

    feat: Add AuthorizedOnlyAttribute

---
+++ Drive/Security/AuthorizedOnlyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drive.Security
+{
+    public class AuthorizedOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (SessionManager.IsValidUser == false)
+                filterContext.Result = new RedirectResult("~/User/Login");
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
