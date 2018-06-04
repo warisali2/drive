08fe76f9223ee864aeea6582e1eda8619de9cb30
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 16:47:25 +0500

    feat: Add saveFolder() in drive-folder

--- Drive.API/Scripts/drive-folder.js
+++ Drive.API/Scripts/drive-folder.js
@@ -4,14 +4,15 @@ Folder.folders = null;
 Folder.container = $("#folders");
 
 //Makes ajax request to server and places them in folders variable
-Folder.getFoldersFromServer = function () {
+Folder.getFoldersFromServer = function (parentFolderId) {
 
     var settings = {};
 
-    settings.url = "/api/folders/getallofuser/" + userId;
-
+    settings.url = "/api/folders/getall/";
+    settings.data = { userId: userId, parentFolderId: parentFolderId};
     settings.success = function (data) {
         Folder.folders = data;
+        Folder.loadFolders();
     };
 
     settings.error = function () {
@@ -31,15 +32,37 @@ Folder.loadFolders = function () {
         container.append($("<span>").text("NO FOLDERS").css("color", "red"));
     }
     else {
-        Folder.folders.forEach(function (folder) {
+        for(var i = 0; i < Folder.folders.length; i++){
+            var folder = Folder.folders[i];
 
-            var div = $("<div>").css("border", "5px solid black").css("clear", "both");
+            var div = $("<div>").css("border", "2px solid black").css("clear", "both");
 
             var Id = $("<span>").text("Id:" + folder.Id).insertAfter($("<br>"));
             var Name = $("<span>").text("Name:" + folder.Name).insertAfter($("<br>"));
             var CreatedOn = $("<span>").text("CreatedOn:" + folder.CreatedOn).insertAfter($("<br>"));
 
             div.append(Id, Name, CreatedOn);
-        });
+            container.append(div);
+        };
     }
 };
+
+//Saves folder by making appropriate ajax request
+Folder.saveFolder = function (name, parentFolderId) {
+
+    var settings = {};
+
+    settings.url = "/api/folders/save";
+    settings.type = "GET";
+    settings.data = { name: name, userId: userId, parentFolderId: parentFolderId };
+    settings.success = function () {
+        Folder.getFoldersFromServer();
+    };
+
+    settings.error = function (e) {
+        alert("Error could not save folder!");
+        alert(e.responseText);
+    };
+
+    $.ajax(settings);
+}
