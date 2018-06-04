fb075ed4707531f3b15ada238e5b4f37deb19a8b
Waris Ali <bcsf15m014@pucit.edu,pk>
2018-06-04 13:07:18 +0500

    fix: Move Drive MVC project to Drive Web API

--- Drive.API/Controllers/HomeController.cs
+++ Drive.API/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using Drive.DAL;
+using Drive.Entities;
+using Drive.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Drive.API.Controllers
+namespace Drive.Controllers
 {
     public class HomeController : Controller
     {
+        [AuthorizedOnly]
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            FolderDAO folderDAO = new FolderDAO();
+
+            var folders = folderDAO.GetAll();
+            var activeAndRootFolders = (from folder in folders
+                                        where folder.IsActive == true && folder.ParentFolderId == -1 && folder.CreatedBy == SessionManager.User.Id
+                                        select folder) as ICollection<Folder>;
+
+            FileDAO fileDAO = new FileDAO();
+
+            var files = fileDAO.GetAll();
+            var activeAndRootFiles = (from file in files
+                                      where file.IsActive == true && file.ParenFolderId == -1 && file.CreatedBy == SessionManager.User.Id
+                                      select file) as ICollection<File>;
+
+            ViewBag.files = activeAndRootFiles;
+            ViewBag.folder = activeAndRootFolders;
 
             return View();
         }
--- Drive/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Drive.MvcApplication" Language="C#" %>
